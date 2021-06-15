eccd1e6a3ffaf5199d391e392187257dec05c3e6
victor-grajski <victor.grajski@gmail.com>
2021-06-15 16:31:33 -0400

    team page

--- src/components/Team.js
+++ src/components/Team.js
@@ -3,6 +3,231 @@ import React from "react";
 
 export default function Team() {
     return (
-      <div>Team</div>
+      <div class="body-2">
+        <div data-collapse="medium" data-animation="default" data-duration="400" role="banner" class="navbar w-nav">
+          <div class="container w-container">
+            <div class="div-block-11">
+              <a href="index.html" class="nav-link dark w-nav-link">99P Labs x MHCI</a>
+            </div>
+            <nav role="navigation" class="nav-menu w-nav-menu">
+              <a href="are-we-there-yet.html" class="nav-link w-nav-link">are we there yet?</a>
+              <a href="team.html" aria-current="page" class="nav-link w-nav-link w--current">Team</a>
+              <a href="https://medium.com/mhci-99p-labs-capstone" target="_blank" rel="noreferrer" class="nav-link w-nav-link">Blog</a>
+            </nav>
+            <div class="menu-button w-nav-button">
+              <div class="icon w-icon-nav-menu"></div>
+            </div>
+          </div>
+        </div>
+        <div class="section-2">
+          <div class="columns-3 w-row">
+            <div class="w-col w-col-1 w-col-stack w-col-small-small-stack w-col-tiny-tiny-stack"></div>
+            <div class="column-9 w-col w-col-10 w-col-stack w-col-small-small-stack w-col-tiny-tiny-stack">
+              <h1 class="heading-3">Meet the Team</h1>
+            </div>
+            <div class="w-col w-col-1 w-col-stack w-col-small-small-stack w-col-tiny-tiny-stack"></div>
+          </div>
+          <div class="columns-2 w-row">
+            <div class="w-col w-col-1 w-col-stack"></div>
+            <div class="column-7 w-col w-col-6 w-col-stack">
+              <div>
+                <div class="div-block-19">
+                  <div class="div-block-18">
+                    <p class="paragraph-6">We&#x27;re a team of Human-Computer Interaction Master&#x27;s (MHCI) students at Carnegie Mellon University. We&#x27;re not just a team of researchers and designers, we&#x27;re a team of iconoclasts who refuse to accept a status quo where the car has been reduced to an appliance. While we may have tried to boil the ocean and failed, we did manage to get a few drops of wisdom, and we&#x27;re excited to share what we&#x27;ve learned with you!</p>
+                  </div>
+                </div>
+                <div class="div-block-20"></div>
+              </div>
+            </div>
+            <div class="w-col w-col-1 w-col-stack"></div>
+            <div class="column-8 w-col w-col-3 w-col-stack">
+              <div class="div-block-21">
+                <div>
+                  <h3 class="heading-2">Our Road Trip Playlist</h3>
+                </div>
+                <div class="w-embed w-iframe"><iframe src="https://open.spotify.com/embed/playlist/49m5J6XQbayAjiodsrVtTu" width="100%" height="423" frameborder="0" allowtransparency="true" allow="encrypted-media" title="Road Trip Playlist"></iframe></div>
+              </div>
+            </div>
+            <div class="w-col w-col-1 w-col-stack"></div>
+          </div>
+          <a href="#cam" class="link-block-3 w-inline-block"><img src="images/chevron-down_1.svg" loading="lazy" alt="" class="image-5" /></a>
+        </div>
+        <div id="cam" class="section-3">
+          <div class="columns-4 w-row">
+            <div class="w-col w-col-1 w-col-stack w-col-small-small-stack"></div>
+            <div class="column-10 w-col w-col-6 w-col-stack w-col-small-small-stack">
+              <div class="div-block-23"></div>
+              <div class="div-block-22">
+                <div class="div-block-24">
+                  <div class="div-block-26"></div>
+                </div>
+                <div class="div-block-25">
+                  <h2 class="heading-4">Cam Davison</h2>
+                  <h2 class="heading-5 hidden">Product Manager</h2>
+                  <div class="div-block-27">
+                    <a href="https://www.linkedin.com/in/cam-davison-8379573a/" target="_blank" rel="noreferrer" class="link-block w-inline-block"><img src="images/iconmonstr-linkedin-3.png" loading="lazy" width="24" height="24" alt="" class="image-3" /></a>
+                    <a href="#" target="_blank" class="link-block-2 hidden w-inline-block"><img src="images/home-alt.svg" loading="lazy" alt="" class="portfolio-icon" /></a>
+                  </div>
+                  <div class="div-block-28">
+                    <p class="paragraph-5">I have 10 years of industry experience spanning advertising strategy, branding, product management and design. Most recently, I lived in Singapore for three years working on UX and product design with startups. I’m looking forward to finding new and meaningful ways to add value to peoples&#x27; lives via mobility.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="w-col w-col-1 w-col-stack w-col-small-small-stack"></div>
+            <div class="column-11 w-col w-col-3 w-col-stack w-col-small-small-stack">
+              <div class="div-block-23"></div>
+              <div class="div-block-21">
+                <div>
+                  <h3 class="heading-2">Cam&#x27;s Road Trip Anthem</h3>
+                </div>
+                <div class="w-embed w-iframe"><iframe src="https://open.spotify.com/embed/track/6mw4IlMjJq52O8Qa48LuAW" width="100%" height="423" frameborder="0" allowtransparency="true" allow="encrypted-media" title="Cam's Road Trip Anthem"></iframe></div>
+              </div>
+            </div>
+            <div class="w-col w-col-1 w-col-stack w-col-small-small-stack"></div>
+          </div>
+        </div>
+        <div class="section-3">
+          <div class="columns-4 reverse w-row">
+            <div class="w-col w-col-1 w-col-stack w-col-small-small-stack"></div>
+            <div class="column-10 w-col w-col-6 w-col-stack w-col-small-small-stack">
+              <div class="div-block-23"></div>
+              <div class="div-block-22">
+                <div class="div-block-24">
+                  <div class="div-block-26 jenny"></div>
+                </div>
+                <div class="div-block-25">
+                  <h2 class="heading-4">Jenny Ong</h2>
+                  <h2 class="heading-5 hidden">Lead UX Researcher</h2>
+                  <div class="div-block-27">
+                    <a href="https://www.linkedin.com/in/jong220/" target="_blank" rel="noreferrer" class="link-block w-inline-block"><img src="images/iconmonstr-linkedin-3.png" loading="lazy" width="24" height="24" alt="" class="image-3" /></a>
+                    <a href="https://jennyongnotes.com/" target="_blank" rel="noreferrer" class="link-block-2 w-inline-block"><img src="images/home-alt.svg" loading="lazy" alt="" class="portfolio-icon" /></a>
+                  </div>
+                  <div class="div-block-28">
+                    <p class="paragraph-5">Formerly an aspiring corporate lawyer who studied business at Georgetown, I pivoted into the digital innovation space to be a part of an innovation incubator within one of the largest global corporate finance firms. Now studying HCI at Carnegie Mellon, I can’t wait to dive deeper into the future of the mobile social experience with 99P Labs!</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="w-col w-col-1 w-col-stack w-col-small-small-stack"></div>
+            <div class="column-11 w-col w-col-3 w-col-stack w-col-small-small-stack">
+              <div class="div-block-23"></div>
+              <div class="div-block-21">
+                <div>
+                  <h3 class="heading-2">Jenny&#x27;s Road Trip Anthem</h3>
+                </div>
+                <div class="w-embed w-iframe"><iframe src="https://open.spotify.com/embed/track/2PSeBKFabj27ud497FxQQy" width="100%" height="423" frameborder="0" allowtransparency="true" allow="encrypted-media" title="Jenny's Road Trip Anthem"></iframe></div>
+              </div>
+            </div>
+            <div class="w-col w-col-1 w-col-stack w-col-small-small-stack"></div>
+          </div>
+        </div>
+        <div class="section-3">
+          <div class="columns-4 w-row">
+            <div class="w-col w-col-1 w-col-stack w-col-small-small-stack"></div>
+            <div class="column-10 w-col w-col-6 w-col-stack w-col-small-small-stack">
+              <div class="div-block-23"></div>
+              <div class="div-block-22">
+                <div class="div-block-24">
+                  <div class="div-block-26 lauren"></div>
+                </div>
+                <div class="div-block-25">
+                  <h2 class="heading-4">Lauren Hung</h2>
+                  <h2 class="heading-5 hidden">Lead Product Designer</h2>
+                  <div class="div-block-27">
+                    <a href="https://www.linkedin.com/in/lauren-hung-83a96aa4/" target="_blank" rel="noreferrer" class="link-block w-inline-block"><img src="images/iconmonstr-linkedin-3.png" loading="lazy" width="24" height="24" alt="" class="image-3" /></a>
+                    <a href="http://laurenhung.com/" target="_blank" rel="noreferrer" class="link-block-2 w-inline-block"><img src="images/home-alt.svg" loading="lazy" alt="" class="portfolio-icon" /></a>
+                  </div>
+                  <div class="div-block-28">
+                    <p class="paragraph-5">I am a research-driven product designer with a degree in Industrial Design from the Rhode Island School of Design. Before coming to MHCI, I worked as a UX designer in an IOT toy startup and UCLA’s HCI research lab. I’m driven by my curiosity about people’s needs and emerging technologies, and I am looking forward to creating prototypes to explore the future of social interaction in automobiles!</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="w-col w-col-1 w-col-stack w-col-small-small-stack"></div>
+            <div class="column-11 w-col w-col-3 w-col-stack w-col-small-small-stack">
+              <div class="div-block-23"></div>
+              <div class="div-block-21">
+                <div>
+                  <h3 class="heading-2">Lauren&#x27;s Road Trip Anthem</h3>
+                </div>
+                <div class="w-embed w-iframe"><iframe src="https://open.spotify.com/embed/track/0lkQOB949M2gLyut86aJ1b" width="100%" height="423" frameborder="0" allowtransparency="true" allow="encrypted-media" title="Lauren's Road Trip Anthem"></iframe></div>
+              </div>
+            </div>
+            <div class="w-col w-col-1 w-col-stack w-col-small-small-stack"></div>
+          </div>
+        </div>
+        <div class="section-3">
+          <div class="columns-4 reverse w-row">
+            <div class="w-col w-col-1 w-col-stack w-col-small-small-stack"></div>
+            <div class="column-10 w-col w-col-6 w-col-stack w-col-small-small-stack">
+              <div class="div-block-23"></div>
+              <div class="div-block-22">
+                <div class="div-block-24">
+                  <div class="div-block-26 sarah"></div>
+                </div>
+                <div class="div-block-25">
+                  <h2 class="heading-4">Sarah Hand</h2>
+                  <h2 class="heading-5 hidden">Lead UX Researcher</h2>
+                  <div class="div-block-27">
+                    <a href="https://www.linkedin.com/in/sarahjessamynhand/" target="_blank" rel="noreferrer" class="link-block w-inline-block"><img src="images/iconmonstr-linkedin-3.png" loading="lazy" width="24" height="24" alt="" class="image-3" /></a>
+                    <a href="https://sarahj930.github.io/sjh_portfolio/#/" target="_blank" rel="noreferrer" class="link-block-2 w-inline-block"><img src="images/home-alt.svg" loading="lazy" alt="" class="portfolio-icon" /></a>
+                  </div>
+                  <div class="div-block-28">
+                    <p class="paragraph-5">As a recent graduate in Psychology and Computer Science from UNC, I am interested in how human-centered design can support human’s well-being and bring about positive societal change. I came to MHCI to build my UX research and design skills before beginning my career in industry. I&#x27;m excited to explore technology-assisted social interaction from a novel perspective!</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="w-col w-col-1 w-col-stack w-col-small-small-stack"></div>
+            <div class="column-11 w-col w-col-3 w-col-stack w-col-small-small-stack">
+              <div class="div-block-23"></div>
+              <div class="div-block-21">
+                <div>
+                  <h3 class="heading-2">Sarah&#x27;s Road Trip Anthem</h3>
+                </div>
+                <div class="w-embed w-iframe"><iframe src="https://open.spotify.com/embed/track/39q7xibBdRboeMKUbZEB6g" width="100%" height="423" frameborder="0" allowtransparency="true" allow="encrypted-media" title="Sarah's Road Trip Anthem"></iframe></div>
+              </div>
+            </div>
+            <div class="w-col w-col-1 w-col-stack w-col-small-small-stack"></div>
+          </div>
+        </div>
+        <div class="section-3">
+          <div class="columns-4 w-row">
+            <div class="w-col w-col-1 w-col-stack w-col-small-small-stack"></div>
+            <div class="column-10 w-col w-col-6 w-col-stack w-col-small-small-stack">
+              <div class="div-block-23"></div>
+              <div class="div-block-22">
+                <div class="div-block-24">
+                  <div class="div-block-26 victor"></div>
+                </div>
+                <div class="div-block-25">
+                  <h2 class="heading-4">Victor Grajski</h2>
+                  <h2 class="heading-5 hidden">Design Technologist</h2>
+                  <div class="div-block-27">
+                    <a href="https://www.linkedin.com/in/victor-grajski/" target="_blank" rel="noreferrer" class="link-block w-inline-block"><img src="images/iconmonstr-linkedin-3.png" loading="lazy" width="24" height="24" alt="" class="image-3" /></a>
+                    <a href="https://victor-grajski.github.io" target="_blank" rel="noreferrer" class="link-block-2 w-inline-block"><img src="images/home-alt.svg" loading="lazy" alt="" class="portfolio-icon" /></a>
+                  </div>
+                  <div class="div-block-28">
+                    <p class="paragraph-5">Before coming to Carnegie Mellon to sharpen my UX skills, I worked in a variety of product development roles (Product Manager/Software Engineer) in companies of various sizes (startups/large companies), and I received a Master’s in Information Science from UC Berkeley. As an automotive enthusiast, I’m excited to work with 99P Labs designing the future of social interaction in automobiles!</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="w-col w-col-1 w-col-stack w-col-small-small-stack"></div>
+            <div class="column-11 w-col w-col-3 w-col-stack w-col-small-small-stack">
+              <div class="div-block-23"></div>
+              <div class="div-block-21">
+                <div>
+                  <h3 class="heading-2">Victor&#x27;s Road Trip Anthem</h3>
+                </div>
+                <div class="w-embed w-iframe"><iframe src="https://open.spotify.com/embed/track/0t6DAi9KEiaWOtgTTXGhD2" width="100%" height="423" frameborder="0" allowtransparency="true" allow="encrypted-media" title="Victor's Road Trip Anthem"></iframe></div>
+              </div>
+            </div>
+            <div class="w-col w-col-1 w-col-stack w-col-small-small-stack"></div>
+          </div>
+        </div>
+        <div class="section-4"></div>
+      </div>
     );
   }
